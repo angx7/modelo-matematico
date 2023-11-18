d1c76edeeff4a4d3e1c1c11e512f930e33811263
Ángel Becerra <angelmirocu@gmail.com>
2023-11-18 15:33:03 -0600

    update

--- reloj.java
+++ reloj.java
@@ -10,31 +10,39 @@ public class reloj {
         System.out.println("Ingresa los minutos");
         int minini = read.nextInt();
 
-        // System.out.println("Ingresa la hora de fin");
-        // int horai00i = read.nextInt();
-        // System.out.println("Ingresa los minutos");
-        // int minini = read.nextInt();
-        double triangulo;
-        if (horaini >= 0 && horaini <= 23) {
-            if (horaini > 12) {
-                horaini -= 12;
-            }
-            triangulo = ((horaini * 60) + minini) * 0.5;
-            double circulo = minini * 6;
-            double rayon = triangulo - circulo;
-            if (rayon < 0) {
-                rayon *= -1;
-            }
+        System.out.println("Ingresa la hora de fin");
+        int horafin = read.nextInt();
+        System.out.println("Ingresa los minutos");
+        int minfin = read.nextInt();
 
-            if (rayon >= 89.5 && rayon <= 90.5) {
-                Horas += horaini + ":" + minini;
-            } else if (rayon >= 269.5 && rayon <= 270.5) {
-                Horas += horaini + ":" + minini;
-            } else {
-                System.out.println("no");
+        for (int i = horaini; i <= horafin; i++) {
+            for (int j = minini; j < 60; j++) {
+                System.out.println(i + ":" + j);
             }
-            System.out.println(Horas);
-            read.close();
+            minini = 0;
         }
+
+        // double triangulo;
+        // if (horaini >= 0 && horaini <= 23) {
+        // if (horaini > 12) {
+        // horaini -= 12;
+        // }
+        // triangulo = ((horaini * 60) + minini) * 0.5;
+        // double circulo = minini * 6;
+        // double rayon = triangulo - circulo;
+        // if (rayon < 0) {
+        // rayon *= -1;
+        // }
+
+        // if (rayon >= 89.5 && rayon <= 90.5) {
+        // Horas += horaini + ":" + minini;
+        // } else if (rayon >= 269.5 && rayon <= 270.5) {
+        // Horas += horaini + ":" + minini;
+        // } else {
+        // System.out.println("no");
+        // }
+        // System.out.println(Horas);
+        // read.close();
+        // }
     }
 }
