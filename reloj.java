ffd05a64e0db6185af2a9f9807577b5f0fb5387a
Ángel Becerra <angelmirocu@gmail.com>
2023-11-18 20:17:44 -0600

    Actualizacion

--- reloj.java
+++ reloj.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class reloj {
 
     public static void main(String[] args) {
-       Scanner read = new Scanner(System.in);
+        Scanner read = new Scanner(System.in);
         String Horas = "";
         System.out.println("Ingresa la hora de inicio");
         int horaini = read.nextInt();
@@ -16,62 +16,62 @@ public class reloj {
         int minfin = read.nextInt();
 
         for (int i = horaini; i < horafin; i++) {
-          if(horaini<horafin){
-             for (int j = minini; j < 60 && j > 0; j++) {
-                
-            //inicio
-         double triangulo;
-        if (i >= 0 && i <= 23) {
-         if (i > 12) {
-         horaini -= 12;
-         }
-         triangulo = ((i * 60) + j) * 0.5;
-         double circulo = j * 6;
-         double rayon = triangulo - circulo;
-         if (rayon < 0) {
-         rayon *= -1;
-         }
+            if (horaini < horafin) {
+                for (int j = minini; j < 60 && j > 0; j++) {
 
-         if (rayon >= 89.5 && rayon <= 90.5) {
-         Horas += i + ":" + j + " ";
-         } else if (rayon >= 269.5 && rayon <= 270.5) {
-         Horas += i + ":" + j + " ";
-         } 
-         //fin
-             
-             }
-             minini = 0;
-          }
+                    // inicio
+                    double triangulo;
+                    if (i >= 0 && i <= 23) {
+                        if (i > 12) {
+                            horaini -= 12;
+                        }
+                        triangulo = ((i * 60) + j) * 0.5;
+                        double circulo = j * 6;
+                        double rayon = triangulo - circulo;
+                        if (rayon < 0) {
+                            rayon *= -1;
+                        }
+
+                        if (rayon >= 89.5 && rayon <= 90.5) {
+                            Horas += i + ":" + j + " ";
+                        } else if (rayon >= 269.5 && rayon <= 270.5) {
+                            Horas += i + ":" + j + " ";
+                        }
+                        // fin
+
+                    }
+                    minini = 0;
+                }
+            }
         }
+
+        for (int i = horafin; i <= horafin; i++) {
+            for (int j = minini; j <= minfin; j++) {
+
+                // inicio
+                double triangulo;
+                if (i >= 0 && i <= 23) {
+                    if (i > 12) {
+                        i -= 12;
+                    }
+                    triangulo = ((i * 60) + j) * 0.5;
+                    double circulo = j * 6;
+                    double rayon = triangulo - circulo;
+                    if (rayon < 0) {
+                        rayon *= -1;
+                    }
+
+                    if (rayon >= 89.5 && rayon <= 90.5) {
+                        Horas += i + ":" + j;
+                    } else if (rayon >= 269.5 && rayon <= 270.5) {
+                        Horas += i + ":" + j + " ";
+                    }
+                    //
+
+                }
+            }
+
+            read.close();
         }
-    
-         for (int i = horafin; i <= horafin; i++) {
-             for (int j = minini; j <= minfin; j++) {
-
-                 //inicio
-         double triangulo;
-        if (i >= 0 && i <= 23) {
-         if (i > 12) {
-         i -= 12;
-         }
-         triangulo = ((i * 60) + j) * 0.5;
-         double circulo = j * 6;
-         double rayon = triangulo - circulo;
-         if (rayon < 0) {
-         rayon *= -1;
-         }
-
-         if (rayon >= 89.5 && rayon <= 90.5) {
-         Horas += i + ":" + j;
-         } else if (rayon >= 269.5 && rayon <= 270.5) {
-         Horas += i + ":" + j + " ";
-         } 
-         //
-                 
-             }
-          }
-        
-                 read.close();
-         }
     }
 }
