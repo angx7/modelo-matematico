fe4c5149f129520717680838f755de0cd781f475
Ángel Becerra <angelmirocu@gmail.com>
2023-11-20 23:29:03 -0600

    Actualizado sin errores

--- reloj.java
+++ reloj.java
@@ -66,10 +66,9 @@ public class reloj {
                 }
             }
         } else {
-            int horafindinamica = 24, minfindinamico = 60;
-            for (int i = horaini; i < horafindinamica; i++) {
-                if (horaini < horafindinamica) {
-                    for (int j = minini; j < minfindinamico; j++) {
+            for (int i = horaini; i < horafin; i++) {
+                if (horaini < horafin) {
+                    for (int j = minini; j < 60; j++) {
                         // inicio
                         double triangulo;
                         if (i >= 0 && i <= 23) {
@@ -101,19 +100,50 @@ public class reloj {
                                 }
                             }
                             // fin
-                            if (i == horafindinamica) {
-                                minfindinamico = minfin;
-                            }
+
                         }
                         minini = 0;
                     }
                 }
-                if (i == 24) {
-                    horafindinamica = horafin;
-                    i = -1;
-                    horaini = i;
-                }
             }
+            // Itera la última parte
+            for (int i = horafin; i <= horafin; i++) {
+                for (int j = minini; j <= minfin; j++) {
+                    horafin = i;
+                    // inicio
+                    double triangulo;
+                    if (i >= 0 && i <= 23) {
+                        if (i > 12) {
+                            horafin -= 12;
+                        }
+                        triangulo = ((horafin * 60) + j) * 0.5;
+                        double circulo = j * 6;
+                        double rayon = triangulo - circulo;
+                        if (rayon < 0) {
+                            rayon *= -1;
+                        }
+
+                        if (rayon >= 89.5 && rayon <= 90.5) {
+                            if (i >= 12) {
+                                Horas += "\t" + i + ":" + j + " PM\n";
+                            } else {
+                                Horas += "\t" + i + ":" + j + " AM\n";
+
+                            }
+                        } else if (rayon >= 269.5 && rayon <= 270.5) {
+                            if (i >= 12) {
+                                Horas += "\t" + i + ":" + j + " PM\n";
+                            } else {
+                                Horas += "\t" + i + ":" + j + " AM\n";
+
+                            }
+                        }
+                        // fin
+                    }
+                }
+
+            }
+
         }
 
         System.out.println(
